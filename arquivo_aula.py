2acbbb4fca4b46c588e8bcca48e6e15824676ffc
Tamires Ana <tamiresanap@outlook.com>
2024-06-15 17:53:25 -0300

    adiona

---
+++ arquivo_aula.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+altura = float(input("Digite sua altura em metros: "))
+idade = int(input("Digite sua idade: "))
+
+print(type(idade))  # Apenas para verificar o tipo de dado
+print(type(altura))  # Apenas para verificar o tipo de dado
+
+
+print(f"O nome do usuario é {nome}, sua altura em metros é {altura}, e sua idade é {idade} anos.")
